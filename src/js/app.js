9b8868cd8c0a2704e3282a277b9140d95b5a36e3
Синан Рамадан <androidmp37@gmail.com>
2022-12-06 02:50:10 +0200

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,12 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const article ="<article class=\"message\"></article><article class=\"message\"></article><article class=\"message\"></article><article class=\"message\"></article><article class=\"message\"></article>";
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
+ var p = document.getElementsByClassName("message");
+ var text = document.createTextNode("New message");
+ paragraph.appendChild(text);
 });
